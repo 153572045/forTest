669165aafc1245752fb0699660fd05963e031045
chenxiaoxian7 <chenxiaoxian7@jd.com>
2022-02-21 23:29:04 +0800

    start test

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,22 @@
+package org.example;
+
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+
+
+
+
+    }
+
+
+}
